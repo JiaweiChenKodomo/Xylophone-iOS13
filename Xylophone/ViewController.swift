6dc58671dad26750ef5a143eecda1ea1300196ff
JiaweiChenKodomo <41921333+JiaweiChenKodomo@users.noreply.github.com>
2023-01-16 16:19:16 -0800

    finished

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(200), execute: {
+            sender.alpha = 1.0
+        })
+        
+        
+        playSound(title: (sender.titleLabel?.text)!)
+        
+        
+        
+    }
+    
+    func playSound(title: String) {
+        
+        guard let path = Bundle.main.path(forResource: title, ofType:"wav") else {
+            return }
+        
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
     
 
 }
